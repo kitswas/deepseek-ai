22e459e52789f33d3bb8e091165ac68b771ce6d4
kitswas <90329875+kitswas@users.noreply.github.com>
2025-01-29 15:26:32 +0530

    Improved conversation handling, additional params

--- deepseek/deepseek_ai.py
+++ deepseek/deepseek_ai.py
@@ -1,9 +1,11 @@
-from pprint import pformat, pprint
+from pprint import pformat
 import sys
 import gradio as gr
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 import torch
 from threading import Thread
+
+max_format_width = 120
 
 model_source = "huggingface"
 
@@ -30,10 +32,17 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}", file=sys.stderr)
 model.to(device)
 
+prev_context = ""
+
 
 # Define the generation function
 def generate_text(
-    message: str, history: list, temperature: float = 0.6, max_new_tokens: int = 1024
+    message: str,
+    history: list,
+    temperature: float,
+    top_p: float,
+    max_new_tokens: int,
+    system_context: str,
 ):
     """
     Args:
@@ -41,34 +50,43 @@ def generate_text(
         history (list): The conversation history used by ChatInterface.
         temperature (float): The temperature for generating the response.
         max_new_tokens (int): The maximum number of new tokens to generate.
+        system_context (str): The system context to guide the response generation.
     """
-    # Options
-    # num_return_sequences = 1
-    top_p = 0.95
-    top_k = 50
+    if not message:
+        yield "Please provide a message."
 
-    max_conversation_length = 128
+    global prev_context
+    max_history_size = 16
     conversation = []
-    print(pformat(history), file=sys.stderr)
-    for user, assistant in history:
-        conversation.extend(
-            [
-                {"role": "user", "content": user},
-                {"role": "assistant", "content": assistant},
-            ]
-        )
+    if len(history) > max_history_size:  # Keep the history size in check
+        history = history[:max_history_size]
+    conversation.extend(history)
+    print(
+        f"Conversation:\n{pformat(conversation, width=max_format_width)}",
+        file=sys.stderr,
+    )
+    print(f"Message: {pformat(message, width=max_format_width)}", file=sys.stderr)
+    if len(conversation) == 0 or system_context != prev_context:
+        message = system_context + "\n" + message
+        prev_context = system_context
     conversation.append({"role": "user", "content": message})
-    if len(conversation) > max_conversation_length:
-        conversation = conversation[:max_conversation_length]
+    conversation.append({"role": "assistant", "content": ""})
 
-    input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt").to(
-        model.device
+    print(
+        f"Input options: temperature={temperature}, top_p={top_p}, max_new_tokens={max_new_tokens}",
+        file=sys.stderr,
+    )
+
+    input_ids = tokenizer.apply_chat_template(
+        conversation,
+        return_tensors="pt",
+        add_generation_prompt=True,
     )
 
     input_ids = input_ids.to(device)
 
     streamer = TextIteratorStreamer(
-        tokenizer, timeout=15.0, skip_prompt=True, skip_special_tokens=True
+        tokenizer, timeout=None, skip_prompt=True, skip_special_tokens=True
     )
 
     generated_kwargs = dict(
@@ -77,11 +95,10 @@ def generate_text(
         max_new_tokens=max_new_tokens,
         do_sample=True,
         temperature=temperature,
-        top_k=top_k,
         top_p=top_p,
-        # num_return_sequences=num_return_sequences,
         eos_token_id=tokenizer.eos_token_id,
         pad_token_id=tokenizer.pad_token_id,
+        attention_mask=input_ids.ne(tokenizer.eos_token_id),
     )
     # Prevent crashing when the temperature is 0.
     if temperature == 0:
@@ -91,33 +108,43 @@ def generate_text(
     t.start()
 
     outputs = []
+    print(f"Output: {pformat(outputs, width=max_format_width)}", file=sys.stderr)
     for text in streamer:
         outputs.append(text)
-        # print(outputs)
         yield "".join(outputs)
 
 
-def generate_multimodal(inputs: dict, options: list[dict]):
+def generate_multimodal(inputs: dict, history: list[dict]):
     """
     the first argument of fn should accept not a str message but a dict message with keys "text" and "files"
     """
 
-    return generate_text(inputs["text"], options)
+    text = generate_text(inputs["text"], history)
+    return text
 
 
 # Create the Gradio interface
 interface = gr.ChatInterface(
     fn=generate_text,
+    show_progress="full",
     additional_inputs_accordion=gr.Accordion(
         label="⚙️ Parameters", open=False, render=False
     ),
     additional_inputs=[
         gr.Slider(
-            minimum=0,
-            maximum=1,
-            step=0.1,
+            minimum=0.5,
+            maximum=0.7,
+            step=0.05,
             value=0.6,
             label="Temperature",
+            render=False,
+        ),
+        gr.Slider(
+            minimum=0.1,
+            maximum=1,
+            step=0.05,
+            value=0.95,
+            label="Top p",
             render=False,
         ),
         gr.Slider(
@@ -128,24 +155,34 @@ interface = gr.ChatInterface(
             label="Max new tokens",
             render=False,
         ),
+        gr.Textbox(
+            label="System context",
+            placeholder="Provide background information for the bot.",
+            value="You are a helpful assistant that answers questions in a friendly and concise manner.",
+            render=False,
+        ),
     ],
-    cache_mode="lazy",
-    cache_examples=True,
+    # cache_mode="lazy",
+    # cache_examples=True,
     examples=[
         [
             "What can you do?",
             0.6,
-            128,
+            0.9,
+            256,
+            "You are a helpful assistant that answers questions in a friendly and concise manner.",
         ],
         [
             "What is 2+2?",
             0.5,
-            16,
+            0.95,
+            128,
+            "You are a math expert who solves problems using formal methods. Please reason step by step, and put your final answer within \\boxed{}.",
         ],
     ],
     type="messages",
-    title="DeepSeek AI",
-    description="This is a demo of DeepSeek AI. It is not an official project.",
+    title="DeepSeek AI on local machine",
+    description=f"Running {model_name} on {device}.",
 )
 
 # Launch the interface
